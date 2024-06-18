288edce90f8d89faf0711a2035ab59dea45bdaf2
sabiq7392 <sabiqmuhammad98@gmail.com>
2024-06-18 12:40:35 +0700

    praktikum 10

---
+++ praktikum-10/peta03.js
@@ -0,0 +1,91 @@
+const peta03Data = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "kampus": "Kampus A STT-NF",
+        "kota": "Kelapa Dua Depok"
+      },
+      "geometry": {
+        "coordinates": [
+          106.84438234679043,
+          -6.362128898913392
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "kampus": "Kampus B STT-NF",
+        "kota": "Srengseng Sawah Jaksel"
+      },
+      "geometry": {
+        "coordinates": [
+          106.832850,
+          -6.354000
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "kampus": "Kampus UI",
+        "kota": "Depok"
+      },
+      "geometry": {
+        "coordinates": [
+          106.827173293457,
+          -6.360508156726524
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "kampus": "Kampus Univ Pancasila",
+        "kota": "Lenteng Agung Jaksel"
+      },
+      "geometry": {
+        "coordinates": [
+          106.83320876073002,
+          -6.339604749519593 
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "kampus": "Kampus Gunadarma",
+        "kota": "Kelapa Dua Depok"
+      },
+      "geometry": {
+        "coordinates": [
+          106.83352844851582,
+          -6.362461084509841 
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "kampus": "Kampus Polimedia",
+        "kota": "Srengseng Sawah Jaksel"
+      },
+      "geometry": {
+        "coordinates": [
+          106.82704196583607,
+          -6.347022617487573 
+        ],
+        "type": "Point"
+      }
+    }
+  ]
+}
+
+
